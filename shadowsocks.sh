622db1f9a495c3dba8d510ce2a0026f29143a462
H-T-H <67087346+H-T-H@users.noreply.github.com>
2024-04-01 22:02:55 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -11,8 +11,6 @@ tee /etc/shadowsocks-libev/config.json > /dev/null <<EOF
 {
     "server":"0.0.0.0",
     "server_port":1,
-    "local_address": "127.0.0.1",
-    "local_port":1,
     "password":"teddysun.com",
     "timeout":300,
     "method":"aes-256-gcm"
