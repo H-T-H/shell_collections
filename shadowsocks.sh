df9b399d83eb3b4349de4e4b8a1b0c95899a73e3
H-T-H <67087346+H-T-H@users.noreply.github.com>
2024-04-01 21:57:46 +0800

    Add files via upload

---
+++ shadowsocks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 更新系统
+apt update
+
+# 安装 Shadowsocks
+apt install -y shadowsocks-libev
+
+# 创建配置文件
+tee /etc/shadowsocks-libev/config.json > /dev/null <<EOF
+{
+    "server":"0.0.0.0",
+    "server_port":1,
+    "local_address": "127.0.0.1",
+    "local_port":1,
+    "password":"teddysun.com",
+    "timeout":300,
+    "method":"aes-256-gcm"
+}
+EOF
+# 启动 Shadowsocks 服务 
+systemctl restart shadowsocks-libev
+
+# 设置开机自启动
+systemctl enable shadowsocks-libev
